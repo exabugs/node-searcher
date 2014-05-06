ab726e7f86940458d7be0f3cefb51da6ce8c2b75
exabugs <exabugs@gmail.com>
2014-05-06 16:33:40 +0900

    Add Mocha test.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   .idea/workspace.xml
    #	modified:   package.json
    #

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-module.exports = {
+/*
 
-  underscore: require("underscore"),
-  async: require("async"),
-  mongodb: require("mongodb"),
-  text: require('./lib/text')
+ */
 
+"use strict";
+
+var MongoClient = require("mongodb").MongoClient
+  , text = require('./lib/text')
+  ;
+
+function Searcher(url, field) {
+  this.url = url; // 'mongodb://127.0.0.1:27017/test'
+  this.field = field; // ['k', 'c', 'v']
 }
+
+Searcher.prototype.open = function (callback) {
+  MongoClient.connect(this.url, function (err, db) {
+    callback(err, db);
+  });
+};
+
+Searcher.prototype.batch = function (src, condition, callback) {
+  var self = this;
+  this.open(function (err, db) {
+    if (err) {
+      callback(err);
+    } else {
+      text.batch(db, src, condition, self.field, function (err, count) {
+        callback(err, count);
+      });
+    }
+  });
+};
+
+module.exports = Searcher;
 
